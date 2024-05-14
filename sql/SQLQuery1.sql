ae3c934e834000d4473999a72c018403d9f5e5da
raguF3 <raguz.leaa@gmail.com>
2024-05-14 18:46:06 +0200

    dodan primary key

--- sql/SQLQuery1.sql
+++ sql/SQLQuery1.sql
@@ -8,7 +8,7 @@ go
 use loo;
 
 create table history(
-sifra int not null,
+sifra int not null primary key identity (1,1),
 ime varchar (50) not null,
 prezime varchar (50) not null,
 vrijeme datetime
