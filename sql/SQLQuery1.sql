ff99948cd08d26faad0484e30e35c3af3849b0f4
raguF3 <raguz.leaa@gmail.com>
2024-05-14 18:42:36 +0200

    database loo

---
+++ sql/SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿use master;
+go
+drop database if exists loo;
+go
+create database loo;
+go
+
+use loo;
+
+create table history(
+sifra int not null,
+ime varchar (50) not null,
+prezime varchar (50) not null,
+vrijeme datetime
+);
