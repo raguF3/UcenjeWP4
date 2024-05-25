18a5a4aa6145aec4a43b36b21b7e371747923846
raguF3 <raguz.leaa@gmail.com>
2024-05-25 10:55:46 +0200

    Create bookclub1.sql

---
+++ bookclub1.sql
@@ -0,0 +1,30 @@
+﻿use master;
+go
+drop database if exists bookclub;
+go
+create database bookclub;
+go
+use bookclub;
+
+create table knjige (
+sifra int not null primary key identity (1,1),
+naziv varchar (50),
+autor varchar (50)
+);
+
+create table sastanci (
+sifra int not null primary key identity (1,1),
+datum datetime,
+knjiga int,
+mjesto varchar (50)
+);
+
+create database clanovi (
+sifra int not null primary key identity (1,1),
+ime varchar (50),
+prezime varchar (50),
+email varchar (50),
+lozinka int,
+admin_id bit
+);
+
