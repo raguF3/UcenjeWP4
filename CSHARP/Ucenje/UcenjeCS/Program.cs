536643da5578f2bbb2656023764c271007984ddb
Raguž Lea <raguz.leaa@gmail.com>
2024-06-08 00:46:07 +0200

    Update Program.cs

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+*
+*komentar kroz
+*vise redova
+*
+*/
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(7);
+Console.WriteLine(true);
+Console.WriteLine("Lea Raguž");
