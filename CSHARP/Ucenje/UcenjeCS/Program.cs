48b7e71611b84bc0ca0b475eabaacec539e667e3
Raguž Lea <raguz.leaa@gmail.com>
2024-06-10 23:27:57 +0200

    pozivi pojedinih djelova koda

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,12 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using UcenjeCS;
+
+
+E01Z1.Izvedi();
+//E02VarijableTipoviPodatakaOperatori.Izvedi();
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
 /*
 *
 *komentar kroz
 *vise redova
 *
 */
-
+/*
 Console.WriteLine("Hello, World!");
 Console.WriteLine(7);
 Console.WriteLine(true);
 Console.WriteLine("Lea Raguž");
+*/
+
