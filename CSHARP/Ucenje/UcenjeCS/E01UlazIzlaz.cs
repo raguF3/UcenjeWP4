f85d4049568d11aaec93464e68340eff0dcb68d1
Raguž Lea <raguz.leaa@gmail.com>
2024-06-10 23:29:02 +0200

    osnove ulaza i izlaaza podataka csharp

---
+++ CSHARP/Ucenje/UcenjeCS/E01UlazIzlaz.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace UcenjeCS
+{
+    internal class E01UlazIzlaz
+    {
+        
+        public static void Izvedi ()
+        {
+
+            string ime;
+            Console.Write("Unesi ime: ");
+            ime = Console.ReadLine();
+            Console.WriteLine(ime);
+
+            //unijeti adresu i grad te ispisati jedno ispod drugog adresu i grad
+
+            string adresa;
+            Console.WriteLine("Unesi adresu: ");
+            adresa = Console.ReadLine();
+            Console.WriteLine(adresa);
+
+            string grad;
+            Console.WriteLine("Unesi grad: ");
+            grad = Console.ReadLine();
+            Console.WriteLine(grad);
+
+        }
+
+    }
+}
+
