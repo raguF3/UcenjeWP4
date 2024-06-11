fbb27cd5dbae4fa8dfad633c1c48b9ab39303be1
Raguž Lea <168165241+raguF3@users.noreply.github.com>
2024-06-11 16:57:23 +0200

    csharp

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * 
+ * komentar kroz
+ * više redova
+ * 
+ */
+using System.Threading.Channels;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(7);
+Console.WriteLine(true);
+
+Console.WriteLine("Lea");
