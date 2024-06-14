f4d6120477a027c3bc2ec2689d07144212119520
Raguž Lea <raguz.leaa@gmail.com>
2024-06-15 00:26:40 +0200

    vježba

--- CSHARP/Ucenje/UcenjeCS/E04Z1.cs
+++ CSHARP/Ucenje/UcenjeCS/E04Z1.cs
@@ -2,6 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
+namespace UcenjeCS
+{
+    internal class E04Z1
+    {
+        public static void Izvedi()
+        {
+            // 1-7 Pon-Ned
+            int broj;
 
+            Console.WriteLine("Unesi broj između 1 i 7: ");
+            broj = int.Parse(Console.ReadLine());
+
+            switch (broj)
+                {
+                case 1:
+                    Console.WriteLine("Ponedjeljak");
+                    break;
+                    case 2:
+                    Console.WriteLine("Utorak");
+                    break;
+                    case 3:
+                    Console.WriteLine("Srijeda");
+                    break;  
+                    case 4:
+                    Console.WriteLine("Četvrtak");
+                    break;
+                    case 5:
+                    Console.WriteLine("Petak");
+                    break;
+                    case 6:
+                    Console.WriteLine("Subota");
+                    break;
+                    case 7:
+                    Console.WriteLine("Nedjelja");
+                    break;
+                default:
+                    Console.WriteLine("Neodređeno");
+                    break;
+            }
+
+           
+        }
+
+    }
+}
--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -4,17 +4,17 @@
 using UcenjeCS;
 
 
-E03Z2.Izvedi();
-
+//E03Z2.Izvedi();
+//E01UlazIzlaz.Izvedi();
 //E05Nizovi.Izvedi();
 //E04UvjetnoGranjanjeSwitch.Izvedi();
 //E03UvjetnoGranjanjeIf.Izvedi();
 //E01Z2.Izvedi();
 //E02VarijableTipoviPodatakaOperatori.Izvedi();
-
+//E04UvjetnoGranjanjeSwitch.Izvedi();
 //E02VarijableTipoviPodatakaOperatori.Izvedi();
 //E05Nizovi.Izvedi();
-//E04Z1.Izvedi();
+E04Z1.Izvedi();
 //E04UvjetnoGranjanjeSwitch.Izvedi();
 //E03Z1.Izvedi();
 //E03UvjetnoGranjanjeIf.Izvedi();
