d55979a149461a4b67045529105dc5ecd6f68f93
Raguž Lea <raguz.leaa@gmail.com>
2024-06-23 00:10:28 +0200

    forpetlja

---
+++ CSHARP/Ucenje/UcenjeCS/E06ForPetlja.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E06ForPetlja
+    {
+        internal static void Izvedi()
+        {
+            //ispiši 10x osijek
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+            Console.WriteLine("Osijek");
+
+            Console.WriteLine("*****************");
+
+            for (int i = 0; i<10; i++)
+            {
+                Console.WriteLine("Osijek");
+            }
+
+            Console.WriteLine("******************");
+
+            for (int i = 0; i<10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("*********************");
+            int suma = 0;
+            for (int i = 0; i<10; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine(suma);
+
+            Console.WriteLine("*********************");
+            //ispiši prvih sto brojeva
+
+            int suma1= 0;
+            for ( int i = 1; i<=100;i++)
+            {
+                suma1 += 1;
+            }
+            Console.WriteLine(suma1);
+
+            Console.WriteLine("*******************");
+            //ispiši brojeve od 3 do 29
+            //loše rješenje
+
+            for(int i = 4; i<29; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("***********************");
+            //dobro rješenje
+            for(int i = 3; i<29; i++)
+            {
+                if(i % 2 ==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -13,8 +13,8 @@ using UcenjeCS;
 
 
 
-
-Vježba123.Izvedi();
+E06ForPetlja.Izvedi();
+//ježba123.Izvedi();
 
 
 //E05Z3.Izvedi();
@@ -23,7 +23,7 @@ Vježba123.Izvedi();
 //E01Z7.Izvedi();
 //E01Z8.Izvedi();
 //E01Z9.Izvedi();
-E03Z3.Izvedi();
+//E03Z3.Izvedi();
 //E03Z4.Izvedi();
 
 
