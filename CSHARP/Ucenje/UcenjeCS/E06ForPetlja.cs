9691ece544784c276fa3a116d170c6020f49d975
Raguž Lea <raguz.leaa@gmail.com>
2024-06-24 23:52:45 +0200

    Update E06ForPetlja.cs

--- CSHARP/Ucenje/UcenjeCS/E06ForPetlja.cs
+++ CSHARP/Ucenje/UcenjeCS/E06ForPetlja.cs
@@ -10,6 +10,12 @@ namespace UcenjeCS
     {
         internal static void Izvedi()
         {
+            Izvedi(0);
+        }
+
+        internal static void Izvedi(int v)
+        {
+           
             //ispiši 10x osijek
             Console.WriteLine("Osijek");
             Console.WriteLine("Osijek");
@@ -70,6 +76,71 @@ namespace UcenjeCS
                 if(i % 2 ==0)
                 {
                     Console.WriteLine(i);
+                }
+               
+                int brojod = 3;
+                int brojdo = 29;
+
+                for (int j = brojod; j < brojdo; j++)
+                {
+                    if (j % 2 ==0)
+                    {        
+                        Console.WriteLine(j); 
+                    }
+                    Console.WriteLine("*************************");
+                    int[] niz = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+                    for (int k = 0; k < niz.Length; k++)
+                    {
+                        Console.WriteLine(niz[k]);
+                    }
+
+
+                    Console.WriteLine("*************************");
+                    //ispiši sve boje od 20 do 10
+
+                    for (int p =20; p>=10; p--)
+                    {
+                        Console.WriteLine(p);
+                    }
+
+                    Console.WriteLine("*************************");
+
+
+                    // iapiši sve prim brojeve od 2 do 50
+                    int ukupno = 0;
+                    bool prim;
+                    for (int h = 2; h<1400  ; h++)
+                    {
+                        prim = true;
+                        for (int o =2; o<h; o++)
+                        {
+                           // Console.WriteLine("{0} % {1} = {2}",h,o,h%o);
+                            if(h%o==0)
+                            {
+                                prim = false; // 2000000 nije prim broj
+                                break;
+                            }
+
+                        }
+                        if(!prim)
+                        {
+                            ukupno++;
+                            //Console.WriteLine("Nepotrebno izvođenje",+ ++ukupno);
+                        }
+                        if (prim)
+                        {
+                            Console.WriteLine(h);
+                        }
+
+
+                    }
+                    Console.WriteLine("nepottrebno izvodio: " + ukupno);
+
+                    Console.WriteLine("*******************************");
+                    
+
+
                 }
             }
         }
