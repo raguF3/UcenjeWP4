d13d7058049e0a0cc9fc1e7c1c9904886c3d7794
Raguž Lea <raguz.leaa@gmail.com>
2024-06-29 16:21:11 +0200

    do while petlja

---
+++ CSHARP/Ucenje/UcenjeCS/E08DoWhilePetlja.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E08DoWhilePetlja
+    {
+        public static void Izvedi()
+        {
+            // do while izdvaja činjenica da će se barem jednom izvest
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("u do while: " +i); //provjera u uvjeta na kraju pa se izvodi
+
+
+            } while (i > 0);
+
+            while (i > 0)
+            { 
+                Console.WriteLine("u while: " +i); // provjera uvjeta na početku veze pa se ne mora izvesti
+            }
+
+            // svi ostali elementi kao i u for i while 
+        }    
+    }
+}
