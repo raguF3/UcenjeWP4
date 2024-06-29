21ec14f5570be809cc8ae69009fea4c97d20b749
Raguž Lea <raguz.leaa@gmail.com>
2024-06-29 17:30:45 +0200

    trycatch

---
+++ CSHARP/Ucenje/UcenjeCS/E10TryCatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E10TryCatch
+    {
+        public static void Izvedi()
+        {
+
+            int i;
+
+            while (true)
+            {
+                Console.WriteLine("Unesi cijeli pozitivni broj: ");
+                try
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (i > 0 && i < 100)
+                    {
+                        break;
+
+                    }
+                    Console.WriteLine("Uneseni broj nije veći od 0 i manji od 100: ");
+                }
+                catch (Exception ex)
+
+                {
+                    Console.WriteLine("Niste unijeli cijeli pozitivan broj: ");
+                }
+
+            }
+            Console.WriteLine("Unijeli ste broj: "+i); //ovdje si sto posto siguran da imaš cijeli broj
+
+
+        }
+    }
+}
