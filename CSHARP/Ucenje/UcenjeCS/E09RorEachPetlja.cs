63432bae097f57346ae913d3ef3997667e997c71
Raguž Lea <raguz.leaa@gmail.com>
2024-06-29 16:38:17 +0200

    foreach petlja

---
+++ CSHARP/Ucenje/UcenjeCS/E09RorEachPetlja.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E09RorEachPetlja
+    {
+        public static void Izvedi()
+        {
+            // foreach je skraćeni for
+            //ispiši svaki znak riječi jedan ispod drugog
+
+            string rijec = "Osijek";
+
+            for (int i = 0; i<rijec.Length; i++)
+            {
+                Console.WriteLine(rijec[i]);
+
+            }
+
+            foreach(var znak in rijec) //foreach radi s listama, nizovima, riječnicima, stringovima
+            {
+                Console.WriteLine(znak);  // ne ide za brojeve
+
+            }
+        }
+
+    }
+}
