7c5d9fa750156e536eaf924ad7ee49a61f5da678
Raguž Lea <raguz.leaa@gmail.com>
2024-07-09 22:12:59 +0200

    cikli

--- CSHARP/Ucenje/UcenjeCS/Zadaci/CiklicnaTablica.cs
+++ CSHARP/Ucenje/UcenjeCS/Zadaci/CiklicnaTablica.cs
@@ -14,53 +14,77 @@ namespace UcenjeCS.Zadaci
         public static void Izvedi()
         {
 
-            Console.Write("Unesi prvi cijeli broj: ");
-            int redak = int.Parse(Console.ReadLine());
-            Console.Write("Unesi drugi cijelim broj: ");
+            Console.Write("Unesi prvi broj: ");
+            int red = int.Parse(Console.ReadLine());
+            Console.Write("Unesi drugi broj: ");
             int stupac = int.Parse(Console.ReadLine());
 
 
-            int[,] tablica = new int[redak, stupac];
+            int[,] tablica = new int[red, stupac];
+            int broj = red * stupac;
+            int r = 0;
+            int s = 0;
+            string smjer = "lijevo";
 
-            int broj = 1;
-
-            int top = 0, bottom = redak - 1, left = 0, right = stupac - 1;
-
-            while (top <= bottom && left <= right)
+            for (int i = 1; i <= broj; i++)
             {
+                tablica[r, s] = i;
 
-                for (int i = right; i >= left; i--)
-                    tablica[bottom, i] = broj++;
-                bottom--;
-
-
-                for (int i = bottom; i >= top; i--)
-                    tablica[i, left] = broj++;
-                left++;
-
-
-                for (int i = left; i <= right; i++)
-                    tablica[top, i] = broj++;
-                top++;
-
-
-                for (int i = top; i <= bottom; i++)
-                    tablica[i, right] = broj++;
-                right--;
+                switch (smjer)
+                {
+                    case "desno":
+                        s++;
+                        if (s >= stupac || tablica[r, s] != 0)
+                        {
+                            s--;
+                            r++;
+                            smjer = "dolje";
+                        }
+                        break;
+                    case "dolje":
+                        r++;
+                        if (r >= red || tablica[r, s] != 0)
+                        {
+                            r--;
+                            s--;
+                            smjer = "lijevo";
+                        }
+                        break;
+                    case "lijevo":
+                        s--;
+                        if (s < 0 || tablica[r, s] != 0)
+                        {
+                            s++;
+                            r--;
+                            smjer = "gore";
+                        }
+                        break;
+                    case "gore":
+                        r--;
+                        if (r < 0 || tablica[r, s] != 0)
+                        {
+                            r++;
+                            s++;
+                            smjer = "desno";
+                        }
+                        break;
+                }
             }
 
-
-            for (int i = 0; i < redak; i++)
+            for (int j = 0; j < red; j++)
             {
-                for (int j = 0; j < stupac; j++)
+                for (int k = 0; k < stupac; k++)
                 {
-                    Console.Write(tablica[i, j] + "\t");
+                    Console.Write("{0,4}", tablica[j, k]);
                 }
                 Console.WriteLine();
-
             }
-
+            
+            
 
         }
+
+
+        
     }
 }
