79d53cb18f20d12405f87e6cca81ee179aab7b8d
Raguž Lea <raguz.leaa@gmail.com>
2024-08-23 22:41:18 +0200

    Update Knjiga.cs

--- CSHARP/Ucenje/UcenjeCS/LjetniRad/BookClub/Citateljskiklub/Knjiga.cs
+++ CSHARP/Ucenje/UcenjeCS/LjetniRad/BookClub/Citateljskiklub/Knjiga.cs
@@ -6,13 +6,19 @@ using System.Threading.Tasks;
 
 namespace UcenjeCS.LjetniRad.BookClub.Citateljskiklub
 {
-    internal class Knjiga: Entitet
-    
+    internal class Knjiga : Entitet
+
     {
-        public int? sifra {  get; set; }
-        public string? naziv {  get; set; }
-        public string? autor {  get; set; }
+        public int? sifra { get; set; }
+        public string? naziv { get; set; }
+        public string? autor { get; set; }
         public DateTime? godina { get; set; }
-       
+
+
+        public override string ToString()
+        {
+            return "sifra=" + sifra + " ,naziv=" + naziv + ", autor=" + autor + ", godina=" + godina;
+
+        }
     }
 }
