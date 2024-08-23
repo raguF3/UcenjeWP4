02f049c9b498dac5e9cd018c79f38b6c48657a80
Raguž Lea <raguz.leaa@gmail.com>
2024-08-23 23:51:48 +0200

    obrada clanovi

---
+++ CSHARP/Ucenje/UcenjeCS/LjetniRad/BookClub/ObradaClan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UcenjeCS.LjetniRad.BookClub.LjetniRad.BookClub.Citateljskiklub;
+
+namespace UcenjeCS.LjetniRad.BookClub
+{
+    internal class ObradaClan
+    {
+        public List<Clan> Clanovi { get; set; }
+
+
+        public ObradaClan()
+        {
+            Clanovi = new List<Clan>();
+        }
+    }
+
+   
+}
