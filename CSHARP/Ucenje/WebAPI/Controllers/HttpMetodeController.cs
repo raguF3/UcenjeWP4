7fff459d93c012d97267de975053d3c3ebb6f141
Raguž Lea <raguz.leaa@gmail.com>
2024-09-11 21:40:54 +0200

    http

---
+++ CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HttpMetodeController : ControllerBase
+    {
+        //ovdje počinje ruta
+
+        [HttpGet]
+        public string Pozdravi()
+        {
+            return "Hello";
+        }
+
+        // ovdje završava
+
+
+        //ovdje počinje ruta
+        [HttpGet]
+        [Route("Pozdrav")]
+
+        public string Pozdravi(string s)
+        {
+            return "Hello " + s;
+        }
+        // ovdje završava
+
+
+        [HttpGet]
+        [Route("Hello")]
+        public IActionResult Pozdravi(int id, string ime)
+        {
+            return Ok(new {sifra = id, naziv = ime});
+        }
+    }
+}
